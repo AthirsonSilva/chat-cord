26d4ed0e50251e98aaa361133a1ae9ef6f806cd9
Athirson <athirsonarceus@gmail.com>
2023-01-17 19:15:24 -0300

    Feat: displaying new messages on chatbox

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
 const socket = io()
+const chatForm = document.querySelector('#chat-form')
+
+socket.on('message', (message) => {
+	console.log(message)
+
+	outputMessage(message)
+})
+
+chatForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const msg = e.target.elements.msg.value
+
+	socket.emit('chatMessage', msg)
+
+	e.target.elements.msg.value = ''
+})
+
+function outputMessage(message) {
+	const div = document.createElement('div')
+	div.classList.add('message')
+	div.innerHTML = `
+		<p class="meta">Mary <span>9:15pm</span></p>
+		<p class="text">
+			${message}
+		</p>
+	`
+
+	document.querySelector('.chat-messages').appendChild(div)
+}
--- server.js
+++ server.js
@@ -10,10 +10,21 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-io.on('connection', (socket) => {
-	console.log('New client connected...')
+io.on('connection', (connection) => {
+	connection.emit('message', 'Welcome to the chat app')
+
+	// Broadcast when a user connects
+	connection.broadcast.emit('message', `The user has joined the chat`)
+
+	connection.on('disconnect', () => {
+		io.emit('message', `The user has left the chat`)
+	})
+
+	connection.on('chatMessage', (msg) => {
+		io.emit('message', msg)
+	})
 })
 
 server.listen(PORT, () =>
-	console.log(`Server is running on port http://localhost:${PORT}`)
+	console.log(`Server is running on port http://localhost:${PORT}`),
 )
